b350b135ebb28983a831bf27dd4269d5c83a820c
Paul Pinto N <paul3126@Pauls-MacBook-Pro.local>
2021-01-05 08:11:13 +0530

    minor update

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ path = require("path");
 //   );
 // }
 
-const port = 6001;
+const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on ${port}`));
 
 var reguserRouter = require("./routes/regUserRoute");
